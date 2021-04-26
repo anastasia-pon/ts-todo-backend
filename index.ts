7716319cb3ce2b017f4c56f5775f7ea92d60e9b0
anastasia-pon <daedelu@gmail.com>
2021-04-26 13:32:41 +0200

    deployment

--- index.ts
+++ index.ts
@@ -52,7 +52,7 @@ app.use(notFoundHandler);
 const  server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3001'],
     credentials: true
   }
 });
