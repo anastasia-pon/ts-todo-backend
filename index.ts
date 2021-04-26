32b6b25461670b5880102eac0146f2780261c530
anastasia-pon <daedelu@gmail.com>
2021-04-26 15:41:23 +0200

    changed cors

--- index.ts
+++ index.ts
@@ -52,7 +52,7 @@ app.use(notFoundHandler);
 const  server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3001'],
+    origin: ['https://ts-todo-frontend.herokuapp.com'],
     credentials: true
   }
 });
